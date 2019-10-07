bfe31a67ce31d2a59e57f273d1ace459b39c29ab
Breyanna Vass <breyannavass@gmail.com>
2019-10-07 19:59:21 -0400

    Adding customers component to App js

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./customers.css";
+
+class Customers extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Customers</h2>
+      </div>
+    );
+  }
+}
+export default Customers;
