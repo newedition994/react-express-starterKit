6b514dae34efec494d6bfd84e66f7134b504ad2c
Breyanna Vass <breyannavass@gmail.com>
2019-10-07 20:10:42 -0400

    Adding CSS to customers and display the customers

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -23,6 +23,13 @@ class Customers extends Component {
     return (
       <div>
         <h2>Customers</h2>
+        <ul>
+          {this.state.customers.map(customer => (
+            <li key={customer.id}>
+              {customer.firstName} {customer.lastName}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
