76681168d50aaddb676e240de3630d46ff2ae4a9
Breyanna Vass <breyannavass@gmail.com>
2019-10-07 20:04:20 -0400

    updating the customers fetch element

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 import "./customers.css";
 
 class Customers extends Component {
+  constructor() {
+    super();
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/customers")
+      .then(res => res.json())
+      .then(customers =>
+        this.setState({ customers }, () =>
+          console.log("Customers fetched..", customers)
+        )
+      );
+  }
+
   render() {
     return (
       <div>
